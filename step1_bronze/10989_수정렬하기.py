216dc4328425b795c0633c50db021ead61aafeed
성선혁 <steve5535@naver.com>
2025-11-16 23:36:45 +0900

    feat: 10989문제

---
+++ step1_bronze/10989_수정렬하기.py
@@ -0,0 +1,10 @@
+count = [0]*10001 # 값을 저장할 리스트
+num = int(input()) # 정렬할 숫자 개수
+
+for i in range(num):
+    x = int(input())
+    count[x] += 1
+
+for i in range(1, 10001):
+    for j in range(count[i]):
+        print(i)
