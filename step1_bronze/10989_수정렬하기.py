c36ac7d20485350b1e3b06d3d7b370a74f3cc245
성선혁 <steve5535@naver.com>
2025-11-16 23:55:21 +0900

    refactor: 속도 빠르게 코드 계선

--- step1_bronze/10989_수정렬하기.py
+++ step1_bronze/10989_수정렬하기.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 count = [0]*10001 # 값을 저장할 리스트
 num = int(input()) # 정렬할 숫자 개수
 
@@ -7,4 +10,4 @@ for i in range(num):
 
 for i in range(1, 10001):
     for j in range(count[i]):
-        print(i)
+        sys.stdout.write(str(i) + "\n")
