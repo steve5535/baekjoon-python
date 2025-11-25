5fa472618248d99b2db9f672a98b69ae065e6c23
SUNG SEON HYEOK <steve5535@naver.com>
2025-11-25 23:00:29 +0900

    feat: 10809문제

---
+++ step1_bronze/10809_알파벳찾기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+s = input().strip() # 입력받은 문자열 s
+list = [-1] * 26 # 26개 알파벳의 등장 위치 저장 리스트
+
+for idx, ch in enumerate(s): # idx에 현재 문자 인덱스추가 ch에 현재 문자 추가
+    idx_in_alphabet = ord(ch) - ord("a") # 현재 문자가 알파벳에서 몇 번째인지 확인
+    if list[idx_in_alphabet] == -1: # 해당 문자가 처음 등장했으면
+        list[idx_in_alphabet] = idx # 그 위치(인덱스)를 저장
+
+print(*list) # 공백으로 구분해서 출력
