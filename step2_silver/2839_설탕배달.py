eff7e08d05a4f3e6ed2ce98779d63af04308d617
SUNG SEON HYEOK <steve5535@naver.com>
2025-12-01 21:44:20 +0900

    fix: 2839번 문제 주석에 오타 수정

--- step2_silver/2839_설탕배달.py
+++ step2_silver/2839_설탕배달.py
@@ -3,10 +3,10 @@ input = sys.stdin.readline
 
 N = int(input()) # 배달해야하는 킬로그램
 
-five = N // 5 # 5kg 봉지로 얼마나 쓸수 있는지 확인하는 변수
+five = N // 5 # 5kg 봉지로 얼마나 쓸 수 있는지 확인하는 변수
 answer = -1 # 답을 저장할 변수
 
-while five >= 0: # 5kg 봉지 개수가 0이상인 동안 반복ㅈ
+while five >= 0: # 5kg 봉지 개수가 0이상인 동안 반복
     rest = N - five * 5 # 5kg 봉지를 쓴 다음에 설탕 무게(나머지)
     if rest % 3 == 0: # 나머지가 3으로 나누어 떨어지면
         answer = five + (rest // 3) # 답을 5kg 봉지를 사용한 횟수 + 나머지를 3으로 나눈몫으로 저장
