237a1fcf0f150c89d847ee4438c5933ad680a903
SUNG SEON HYEOK <steve5535@naver.com>
2025-12-02 22:51:14 +0900

    feat: 1152번 문제

--- 1152_단어의개수.py
+++ 1152_단어의개수.py
@@ -2,6 +2,6 @@ import sys
 input = sys.stdin.readline
 
 sentence = input().strip().split() # 입력 받은 문장에 처음과 끝에 공백을 제거하고 공백을 기준으로 단어를 나눈 값을 저장함
-sentence_len = len(sentence)
+sentence_len = len(sentence) # 단어의 계수를 저장
 
-print(sentence_len)
+print(sentence_len) # 단어의 개수 출력
