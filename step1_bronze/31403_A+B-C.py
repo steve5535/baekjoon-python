79a8d6ccf79bab8a88dea04d7824337a5a336fe0
SUNG SEON HYEOK <steve5535@naver.com>
2025-12-24 17:43:11 +0900

    feat: 31403번 문제

---
+++ step1_bronze/31403_A+B-C.py
@@ -0,0 +1,7 @@
+A = int(input()) # 입력 받은 값을 정수로 A에 저장
+B = int(input()) # 입력 받은 값을 정수로 B에 저장
+C = int(input())  # 입력 받은 값을 정수로 C에 저장
+str_value = int(str(A)+str(B)) - C # A, B, C를 문자열로 보고 A + B - C 결과값을 저장
+
+print(A + B - C) # A, B, C를 수로 보고 A + B - C 결과값을 출력
+print(str_value) # 문자열로 보고 계산한 값 출력
