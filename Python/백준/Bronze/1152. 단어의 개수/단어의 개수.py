9cc7509205889bf15bf58269328b14b1d1d5c5d9
SUNG SEON HYEOK <steve5535@naver.com>
2026-04-18 21:53:21 +0900

    [Bronze II] Title: 단어의 개수, Time: 48 ms, Memory: 39824 KB -BaekjoonHub

--- Python/백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ Python/백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,2 +1,7 @@
-string = list(input().strip().split()) # 입력받은 값에 얖옆에 공백을 제거하고 공백을 기준으로 나누고 리스트로 저장
-print(len(string)) # 리스트에 길이(단어의 개수)를 출력
+import sys
+input = sys.stdin.readline
+
+sentence = input().strip().split() # 입력 받은 문장에 처음과 끝에 공백을 제거하고 공백을 기준으로 단어를 나눈 값을 저장함
+sentence_len = len(sentence) # 단어의 계수를 저장
+
+print(sentence_len) # 출력
