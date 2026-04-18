7b971a6b225faf09bc0905a9ead029262e5713da
SUNG SEON HYEOK <steve5535@naver.com>
2026-04-18 21:43:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ Python/백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,10 +1,14 @@
-x = int(input())
-y = int(input())
-if x > 0 and y > 0:
-    print(1)
-elif x < 0 and y >0:
-    print(2)
-elif x < 0 and y < 0:
-    print(3)
-elif x > 0 and y < 0:
+import sys
+input = sys.stdin.readline
+
+x = int(input().strip()) # 정수 x를 입력받아 저장
+y = int(input().strip()) # 정수 y를 입력받아 저장
+
+if x > 0 and y > 0: # x와 y가 0보다 크면(제1사분면)
+    print(1) # 1을 출력
+elif x < 0 and y > 0: # x가 0보다 작고 y가 0보다 크면(제2사분면)
+    print(2) # 2을 출력
+elif x < 0 and y < 0: # x와 y가 0보다 작으면(제3사분면)
+    print(3) # 3을 출력
+elif x > 0 and y < 0: # x가 0보다 크고 y가 0보다 작으면(제4사분면)
     print(4)
